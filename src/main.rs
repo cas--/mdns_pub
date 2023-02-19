b832b145323d66b645d4037fdf0ece83a13312f7
Calum Lind <calumlind@gmail.com>
2023-02-19 22:37:31 +0000

    Refactor notify call to use dbus_proxy trait

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
 use std::collections::HashMap;
 use std::error::Error;
 
-use zbus::{zvariant::Value, Connection};
+use zbus::{dbus_proxy, zvariant::Value, Connection};
+
+#[dbus_proxy(
+    interface = "org.freedesktop.Notifications",
+    default_service = "org.freedesktop.Notifications",
+    default_path = "/org/freedesktop/Notifications"
+)]
+trait Notifications {
+    // Call Notify D-Bus method
+    fn notify(
+        &self,
+        app_name: &str,
+        replaces_id: u32,
+        app_icon: &str,
+        summary: &str,
+        body: &str,
+        actions: &[&str],
+        hints: HashMap<&str, &Value<'_>>,
+        expire_timeout: i32,
+    ) -> zbus::Result<u32>;
+}
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let connection = Connection::session().await?;
 
-    let m = connection.call_method(
-        Some("org.freedesktop.Notifications"),
-        "/org/freedesktop/Notifications",
-        Some("org.freedesktop.Notifications"),
-        "Notify",
-        &("my-app", 0u32, "dialog-information", "A summary", "Some body",
-          vec![""; 0], HashMap::<&str, &Value>::new(), 5000),
-    ).await?;
+    let proxy = NotificationsProxy::new(&connection).await?;
 
-    let reply: u32 = m.body().unwrap();
+    let reply = proxy
+        .notify(
+            "my-app",
+            0,
+            "dialog-information",
+            "A summary",
+            "Some body",
+            &[],
+            HashMap::new(),
+            5000,
+        )
+        .await?;
     dbg!(reply);
     Ok(())
 }
