c9e3d433ed459304a9cce793488cb1a48dbeb987
Calum Lind <calumlind@gmail.com>
2023-02-20 12:39:50 +0000

    Refactor Avahi traits into module
    Use zbus_xmlget to generate traits from Avahi server
    Create an Avahi module to keep code clean.
    
    - https://crates.io/crates/zbus_xmlgen

---
+++ src/avahi/mod.rs
@@ -0,0 +1,3 @@
+pub mod server;
+pub mod entry_group;
+
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,15 @@ use std::error::Error;
 
 use zbus::{dbus_proxy, Connection};
 
-#[dbus_proxy(
-    interface = "org.freedesktop.Avahi.Server",
-    default_service = "org.freedesktop.Avahi",
-    default_path = "/"
-)]
-trait AvahiServer {
-    // Call Notify D-Bus method
-    fn GetHostNameFqdn(&self) -> zbus::Result<String>;
-}
+mod avahi;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let connection = Connection::system().await?;
 
-    let avahi_server = AvahiServerProxy::new(&connection).await?;
+    let avahi_server = avahi::server::ServerProxy::new(&connection).await?;
 
-    let reply = avahi_server.GetHostNameFqdn().await?;
+    let reply = avahi_server.get_host_name_fqdn().await?;
     dbg!(reply);
     Ok(())
 }
