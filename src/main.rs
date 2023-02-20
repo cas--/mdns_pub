b6d09066148e69a5535c095299d4969644ef1224
Calum Lind <calumlind@gmail.com>
2023-02-20 17:54:04 +0000

    Add ability to convert domain to rdata

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@ use zbus::Connection;
 
 mod avahi;
 
+fn domain_to_rdata(domain: &str) -> Vec<u8> {
+    // Convert a domain into mDNS data record format (rdata)
+
+    fn label_to_rdata(label: &str) -> Vec<u8> {
+        let mut rlabel = vec![label.len() as u8];
+        rlabel.extend(label.as_bytes().to_vec());
+        rlabel
+    }
+
+    let mut rdata: Vec<u8> = domain
+        .split('.')
+        .flat_map(|label| label_to_rdata(label))
+        .collect();
+    rdata.push(0 as u8);
+    rdata
+}
+
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let connection = Connection::system().await?;
